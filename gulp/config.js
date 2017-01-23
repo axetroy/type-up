5779f08e6e0802ca1a9b9774cc093d7bd19d4026
axetroy <troy450409405@gmail.com>
2017-01-23 22:12:42 +0800

    init

---
+++ gulp/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by axetroy on 17-1-23.
+ */
+let path = require('path');
+
+exports.paths = {
+  src: path.join('src'),
+  dist: path.join('dist'),
+  tmp: path.join('.tmp')
+};
---
+++ gulp/script.js
@@ -0,0 +1,59 @@
+let path = require('path');
+
+let gulp = require('gulp');
+let clean = require('gulp-clean');
+let dart = require("gulp-dart");
+let uglify = require('gulp-uglify');
+let header = require('gulp-header');
+
+let {paths} = require('./config');
+
+function headerStream() {
+  return header(
+    `// ==UserScript==
+// @name          敲起来
+// @description   当你在敲击键盘的时候, 出现相应键位的动画特效
+// @version       0.3.0
+// @author        Axetroy
+// @include       *
+// @grant         none
+// @run-at        document-start
+// @namespace         https://greasyfork.org/zh-CN/users/3400-axetroy
+// @license           The MIT License (MIT); http://opensource.org/licenses/MIT
+// ==/UserScript==
+
+`);
+}
+
+gulp.task('clean', function () {
+  return gulp.src(path.join(paths.dist, '*.*'), {read: false})
+    .pipe(clean());
+});
+
+gulp.task('script', function () {
+  return gulp.src(path.join(paths.src, '*.dart'))
+    .pipe(dart({
+      "dest": paths.dist
+    }))
+    .pipe(headerStream())
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('build', function () {
+  return gulp.src(path.join(paths.src, '*.dart'))
+    .pipe(dart({
+      "dest": paths.dist,
+      "minify": "true"
+    }))
+    .pipe(uglify())
+    .pipe(headerStream())
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('watch', ['clean', 'build'], function () {
+  gulp.watch(path.join(paths.src, '*.dart'), ['build']);
+});
+
+gulp.task('default', ['clean'], function () {
+  // gulp.start('build');
+});
